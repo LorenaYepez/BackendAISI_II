ed5bf367803d2dbfceb98cf189fd05b2bbd1d45e
Lo Yepez <lorenaponce246@gmail.com>
2025-07-20 13:55:53 -0400

    Update libreria

--- backend/apps/usuarios/views.py
+++ backend/apps/usuarios/views.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import random
+import urllib.request
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
